dd0b0a762493f44f22c2d5e88faa033dd8fe8536
uday <udayjp11@gmail.com>
2021-04-29 14:05:07 +0530

    use sqlite db

--- cakeshop_api/settings.py
+++ cakeshop_api/settings.py
@@ -79,30 +79,30 @@ WSGI_APPLICATION = 'cakeshop_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Postgres database connection
 DATABASES = {
     'default': {
-        
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': config('DB_NAME'),
-
-        'USER': config('DB_USER'),
-
-        'PASSWORD': config('DB_PASSWORD'),
-
-        'HOST': config('DB_HOST'),
-
-        'PORT': config('DB_PORT'),
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# Postgres database connection
+# DATABASES = {
+#     'default': {
+        
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': config('DB_NAME'),
+
+#         'USER': config('DB_USER'),
+
+#         'PASSWORD': config('DB_PASSWORD'),
+
+#         'HOST': config('DB_HOST'),
+
+#         'PORT': config('DB_PORT'),
+#     },
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
